456e7e088a76fa8bfef44627007a2c6f7065222d
hanseul <w0w1278@naver.com>
2022-08-09 13:46:10 +0900

    datas

--- main.py
+++ main.py
@@ -13,24 +13,30 @@ results = data['results']
 properties = results[0]["properties"]
 templates = Jinja2Templates(directory='templates')
 
+
+
+#cafename = results[i]["properties]["name"]["title"][0]["plain_text]
+#cafeaddress = results[i]["properties"]["address"]["rich_text"][0]["text"]["content"]
+#cafeurl = results[i]["properties"]["url"]["url"]
+#cafetype = results[i]["properties"]["type"]["rich_text"][0]["text"]["content"]
+
 @app.get("/data/3")
 def geturl1():
     return results[2]["properties"]["name"]["title"][0]["plain_text"]
-
 @app.get("/data/2")
 def geturl1():
-    return results[1]["properties"]
+    return results[2]["properties"]["address"]["rich_text"][0]["text"]["content"]
 
 
 @app.get("/data/1")
 def geturl1():
-    return results[0]
+    return results[2]["properties"]["url"]["url"]
 
 @app.get("/data")
 def geturl():
-    return properties
+    return results[2]["properties"]["type"]["rich_text"][0]["text"]["content"]
 
 @app.get("/")
 def main():
-    return FileResponse("templates/index.html")
+    return results[2]["properties"]
 
