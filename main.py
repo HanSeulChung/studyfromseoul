f13f6176658fa696e38a089257a0b2972588010c
hanseul <w0w1278@naver.com>
2022-08-10 12:07:27 +0900

    idk

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -12,31 +14,37 @@ data = requests.get("https://serverless-api.bell23bella.workers.dev/").json()
 results = data['results']
 properties = results[0]["properties"]
 templates = Jinja2Templates(directory='templates')
+app.mount("/static", StaticFiles(directory="static"), name="static")
+name = results[2]["properties"]["name"]["title"][0]["plain_text"]
 
-
-
-#cafename = results[i]["properties]["name"]["title"][0]["plain_text]
+#cafename = results[i]["properties"]["name"]["title"][0]["plain_text"]
 #cafeaddress = results[i]["properties"]["address"]["rich_text"][0]["text"]["content"]
 #cafeurl = results[i]["properties"]["url"]["url"]
 #cafetype = results[i]["properties"]["type"]["rich_text"][0]["text"]["content"]
 
-@app.get("/data/3")
+class Cafe(BaseModel):
+    name :str
+    address : str
+    type : str
+    url : str
+
+@app.get("/data/5")
+def geturl1():
+    return name
+@app.get("/data/4")
 def geturl1():
     return results[2]["properties"]["name"]["title"][0]["plain_text"]
-@app.get("/data/2")
+@app.get("/data/3")
 def geturl1():
     return results[2]["properties"]["address"]["rich_text"][0]["text"]["content"]
-
-
-@app.get("/data/1")
+@app.get("/data/2")
 def geturl1():
     return results[2]["properties"]["url"]["url"]
-
-@app.get("/data")
+@app.get("/data/1")
 def geturl():
     return results[2]["properties"]["type"]["rich_text"][0]["text"]["content"]
 
-@app.get("/")
-def main():
-    return results[2]["properties"]
+@app.get("/",response_class=HTMLResponse)
+def main(request: Request, cafe :Cafe):
+    return templates.TemplateResponse("index.html", {"request": request, "cafe": cafe})
 
--- test.py
+++ test.py
@@ -1,5 +1,50 @@
+from fastapi import FastAPI
+from fastapi.responses import UJSONResponse,HTMLResponse,RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import requests
+from starlette.requests import Request
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory='templates')
 data = requests.get("https://serverless-api.bell23bella.workers.dev/").json()
-id = data["results"][0]["properties"]["이름"]["id"].plain_text
-print(data)
-print(id)
+results = data['results']
+#name = results[2]["properties"]["name"]["title"][0]["plain_text"]
+
+@app.get("/")
+async def say_helloworld():
+    return "hello world"
+
+@app.get("/items/1", response_class=UJSONResponse)
+async def read_items():
+    name = results[2]["properties"]["name"]["title"][0]["plain_text"]
+    return name
+
+def generate_html_response():
+    html_content = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Work from Seoul</title>
+    </head>
+
+    <body>
+        <h1>Work from Seoul</h1>
+        {% for name in name %}
+        <h2> {{ name }}</h2>
+        {% endfor %}
+        <h3></h3>
+        <h3></h3>
+        <h4></h4>
+        
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
+
+
+@app.get("/cafe", response_class=HTMLResponse)
+async def read_items(request:Request, name: str):
+    return generate_html_response({"request":request, "name":name})
